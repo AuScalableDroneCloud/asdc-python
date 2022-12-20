ac3cdb8775296f9e31f2c2cfb3e27ff40ba2235d
Owen Kaluza <owen.kaluza@monash.edu>
2022-12-20 12:13:17 +1100

    Use service_prefix instead of user-redirect
    user-redirect doesn't work with named servers

--- asdc/server.py
+++ asdc/server.py
@@ -86,12 +86,17 @@ import_doc = """
 <body>
     <h1>ASDC API Request</h3>
     <p>Request processed for {FN}
-    <a href="/user-redirect/lab/tree/{FN}">(Output here)</a>
+    <a href="/{prefix}/lab/tree/{FN}">(Output here)</a>
     </p>
 </body>
 
 </html>
 """
+
+prefix = os.getenv('JUPYTERHUB_SERVICE_PREFIX')
+#user = os.getenv('JUPYTERHUB_USER')
+#base = os.getenv('JUPYTERHUB_URL')
+#server = os.getenv('JUPYTERHUB_SERVER_NAME')
 
 class RequirementsHandler(tornado.web.RequestHandler):
     def get(self):
@@ -117,8 +122,7 @@ class RedirectHandler(tornado.web.RequestHandler):
 
         utils.write_inputs(projects=projects, tasks=tasks)
 
-        return self.redirect(f"/user-redirect/lab/tree/{redirect}")
-
+        return self.redirect(f"/{prefix}/lab/tree/{redirect}")
 
 class ImportHandler(tornado.web.RequestHandler):
     def get(self):
@@ -138,8 +142,8 @@ class ImportHandler(tornado.web.RequestHandler):
 
         script = ""
         if redirect == 'yes':
-            #script = 'window.location.href="/user-redirect/lab/tree/{FN}"'.format(FN=filename)
-            return self.redirect(f"/user-redirect/lab/tree/{filename}")
+            #script = f'window.location.href="/{prefix}/lab/tree/{filename}"'
+            return self.redirect(f"/{prefix}/lab/tree/{filename}")
         else:
             #self.write(import_doc.format(FN=filename, script=script))
             return self.write(import_doc.format(FN=filename, script=""))
@@ -159,7 +163,7 @@ class BrowseHandler(tornado.web.RequestHandler):
                 data = project_dict[PID]
                 if not "name" in data:
                     print("Unexpected response: ", data)
-                    self.redirect(f"/user-redirect/lab/tree/")
+                    self.redirect(f"/{prefix}/lab/tree/")
                 projname = data["name"]
                 projdir = str(PID) + '_' + slugify(project)
                 for t in data["tasks"]:
@@ -168,7 +172,7 @@ class BrowseHandler(tornado.web.RequestHandler):
                             t["name"] = str(t["id"])
                         taskdir = str(idx) + '_' + slugify(t["name"]) # + '_(' + str(t['id'])[0:8] + ')'
                         break
-                return self.redirect(f"/user-redirect/lab/tree/projects/{projdir}/{taskdir}")
+                return self.redirect(f"/{prefix}/lab/tree/projects/{projdir}/{taskdir}")
         else:
             #Can't get name data, just use PID and TID, create symlink first
             tpath = "/mnt/project/{PID}/task/TID"
@@ -176,7 +180,7 @@ class BrowseHandler(tornado.web.RequestHandler):
             os.makedirs(lnpath, exist_ok=True)
             lnpath = os.path.join(lnpath, TID)
             os.symlink(tpath, lnpath)
-            return self.redirect(f"/user-redirect/lab/tree/projects/{PID}/{TID}")
+            return self.redirect(f"/{prefix}/lab/tree/projects/{PID}/{TID}")
 
 # Following page HTML and Javascript from ipyauth
 # https://gitlab.com/oscar6echo/ipyauth
